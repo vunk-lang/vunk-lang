4a5064534366f75588b344ab733237552e6f3aed
Matthias Beyer <mail@beyermatthias.de>
2023-03-12 16:52:17 +0100

    Add Decl::parser() dummy
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- vunk-parser/src/ast/decl.rs
+++ vunk-parser/src/ast/decl.rs
@@ -2,10 +2,15 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use chumsky::prelude::Simple;
+use chumsky::Parser;
+use vunk_lexer::Token;
+
 use crate::ast::def::Def;
 use crate::ast::generic::WhereClause;
 use crate::ast::name::TypeName;
 use crate::ast::name::VariableName;
+use crate::Spanned;
 
 #[derive(Debug)]
 #[cfg_attr(test, derive(PartialEq))]
@@ -13,6 +18,16 @@ pub struct Decl {
     pub lhs: VariableName,
     pub rhs: DeclType,
     pub whereclause: Option<WhereClause>,
+}
+
+impl Decl {
+    pub fn parser(
+    ) -> impl Parser<Spanned<Token>, Spanned<Self>, Error = Simple<Spanned<Token>>> + Clone {
+        let i: Box<dyn Parser<Spanned<Token>, Spanned<Self>, Error = Simple<Spanned<Token>>>> =
+            { unimplemented!() };
+
+        std::sync::Arc::new(i)
+    }
 }
 
 #[derive(Debug)]
