bb4e7bfa8a120e54a31c79b8fbd7d260a2074008
Matthias Beyer <mail@beyermatthias.de>
2023-03-12 16:52:42 +0100

    Add Def::parser() dummy
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- vunk-parser/src/ast/def.rs
+++ vunk-parser/src/ast/def.rs
@@ -2,16 +2,31 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use chumsky::prelude::Simple;
+use chumsky::Parser;
+use vunk_lexer::Token;
+
 use crate::ast::expr::Expr;
 use crate::ast::generic::WhereClause;
 use crate::ast::name::TypeName;
 use crate::ast::name::VariableName;
+use crate::Spanned;
 
 #[derive(Debug)]
 #[cfg_attr(test, derive(PartialEq))]
 pub struct Def {
     pub lhs: VariableName,
     pub rhs: DefRhs,
+}
+
+impl Def {
+    pub fn parser(
+    ) -> impl Parser<Spanned<Token>, Spanned<Self>, Error = Simple<Spanned<Token>>> + Clone {
+        let i: Box<dyn Parser<Spanned<Token>, Spanned<Self>, Error = Simple<Spanned<Token>>>> =
+            { unimplemented!() };
+
+        std::sync::Arc::new(i)
+    }
 }
 
 #[derive(Debug)]
