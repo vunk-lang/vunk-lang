d91b722fb20395f82774fa013c520d270cc99e82
Matthias Beyer <mail@beyermatthias.de>
2023-03-12 16:49:33 +0100

    Add Expr::parser() dummy
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- vunk-parser/src/ast/expr.rs
+++ vunk-parser/src/ast/expr.rs
@@ -2,6 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use chumsky::prelude::Simple;
+use chumsky::Parser;
+use vunk_lexer::Token;
+
 use crate::ast::decl::Decl;
 use crate::ast::def::Def;
 use crate::ast::ifelse::IfElse;
@@ -10,6 +14,7 @@ use crate::ast::literal::Literal;
 use crate::ast::name::VariableName;
 use crate::ast::op::BinaryOp;
 use crate::ast::op::UnaryOp;
+use crate::Spanned;
 
 #[derive(Debug)]
 #[cfg_attr(test, derive(PartialEq))]
@@ -22,4 +27,14 @@ pub enum Expr {
     IfElse(IfElse),
     Decl(Decl),
     Def(Def),
+}
+
+impl Expr {
+    pub fn parser(
+    ) -> impl Parser<Spanned<Token>, Spanned<Self>, Error = Simple<Spanned<Token>>> + Clone {
+        let i: Box<dyn Parser<Spanned<Token>, Spanned<Self>, Error = Simple<Spanned<Token>>>> =
+            { unimplemented!() };
+
+        std::sync::Arc::new(i)
+    }
 }
