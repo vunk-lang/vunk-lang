b4b463eece5d621ed761205899d26283e988738b
Matthias Beyer <mail@beyermatthias.de>
2023-03-13 10:33:42 +0100

    Add VariableName::parser() impl
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- vunk-parser/src/ast/name.rs
+++ vunk-parser/src/ast/name.rs
@@ -2,9 +2,26 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use chumsky::error::Simple;
+use chumsky::select;
+use chumsky::Parser;
+use vunk_lexer::Token;
+
+use crate::Spanned;
+
 #[derive(Debug)]
 #[cfg_attr(test, derive(PartialEq))]
 pub struct VariableName(pub String);
+
+impl VariableName {
+    pub fn parser(
+    ) -> impl Parser<Spanned<Token>, Spanned<VariableName>, Error = Simple<Spanned<Token>>> + Clone
+    {
+        chumsky::select! {
+            (Token::Ident(s), span) => (VariableName(s), span),
+        }
+    }
+}
 
 #[derive(Debug)]
 #[cfg_attr(test, derive(PartialEq))]
