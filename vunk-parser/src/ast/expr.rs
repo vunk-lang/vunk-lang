ef5a92b89ec40aae6466bcb57cb733a5383d12e4
Matthias Beyer <mail@beyermatthias.de>
2023-03-13 09:29:43 +0100

    Add Expr::parser() impl
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- vunk-parser/src/ast/expr.rs
+++ vunk-parser/src/ast/expr.rs
@@ -34,9 +34,80 @@ pub enum Expr {
 impl Expr {
     pub fn parser(
     ) -> impl Parser<Spanned<Token>, Spanned<Self>, Error = Simple<Spanned<Token>>> + Clone {
-        let i: Box<dyn Parser<Spanned<Token>, Spanned<Self>, Error = Simple<Spanned<Token>>>> =
-            { unimplemented!() };
+        chumsky::recursive::recursive(|_tree| {
+            let variable_parser = VariableName::parser().map(|(v, span)| (Expr::Variable(v), span));
 
-        std::sync::Arc::new(i)
+            let unary_parser = UnaryOp::parser()
+                .then(Expr::parser().map(|(e, span)| (Box::new(e), span)))
+                .map(|((op, opspan), (ex, exspan))| {
+                    let span = std::ops::Range {
+                        start: opspan.start,
+                        end: exspan.end,
+                    };
+
+                    let e = Expr::Unary(op, ex);
+                    (e, span)
+                });
+
+            let binary_parser = Expr::parser()
+                .map(|(e, span)| (Box::new(e), span))
+                .then(BinaryOp::parser())
+                .then(Expr::parser().map(|(e, span)| (Box::new(e), span)))
+                .map(|(((exl, _exlspan), (op, _opspan)), (exr, exrspan))| {
+                    let span = std::ops::Range {
+                        start: exrspan.start,
+                        end: exrspan.end,
+                    };
+
+                    let e = Expr::Binary(op, exl, exr);
+                    (e, span)
+                });
+
+            let literal_parser = Literal::parser()
+                .map(|(lit, span)| {
+                    let e = Expr::Literal(lit);
+                    (e, span)
+                });
+
+            let letin_parser = LetIns::parser()
+                .map(|(li, span)| {
+                    let e = Expr::LetIn(li);
+                    (e, span)
+                });
+
+            let ifelse_parser = IfElse::parser()
+                .map(|(ie, span)| {
+                    let e = Expr::IfElse(ie);
+                    (e, span)
+                });
+
+            let module_parser = Module::parser()
+                .map(|(m, span)| {
+                    let e = Expr::Module(m);
+                    (e, span)
+                });
+
+            let decl_parser = Decl::parser()
+                .map(|(d, span)| {
+                    let e = Expr::Decl(d);
+                    (e, span)
+                });
+
+            let def_parser = Def::parser()
+                .map(|(d, span)| {
+                    let e = Expr::Def(d);
+                    (e, span)
+                });
+
+            variable_parser
+                .or(unary_parser)
+                .or(binary_parser)
+                .or(literal_parser)
+                .or(letin_parser)
+                .or(ifelse_parser)
+                .or(module_parser)
+                .or(decl_parser)
+                .or(def_parser)
+        })
     }
 }
