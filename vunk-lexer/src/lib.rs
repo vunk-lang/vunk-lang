4426b41a143d965f939a01f0ee677f985b4a86ff
Matthias Beyer <mail@beyermatthias.de>
2023-03-14 14:33:40 +0100

    Update lexer to chumsky 1.0.0
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- vunk-lexer/src/lib.rs
+++ vunk-lexer/src/lib.rs
@@ -2,29 +2,30 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use chumsky::error::Simple;
-use chumsky::primitive::filter;
+use chumsky::primitive::any;
+use chumsky::primitive::end;
 use chumsky::primitive::just;
+use chumsky::primitive::none_of;
 use chumsky::primitive::one_of;
-use chumsky::primitive::take_until;
 use chumsky::recovery::skip_then_retry_until;
 use chumsky::text;
-use chumsky::text::TextParser;
+use chumsky::text::ident;
 use chumsky::Parser;
+use chumsky::IterParser;
 
-pub type Span = std::ops::Range<usize>;
-pub type Spanned<T> = (T, Span);
+type Span = chumsky::span::SimpleSpan<usize>;
+type Spanned<T> = (T, Span);
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub enum Token {
-    Ident(String),
+pub enum Token<'src> {
+    Ident(&'src str),
 
     Arrow,
     Ctrl(char),
-    Op(String),
+    Op(&'src str),
 
-    Num(String),
-    Str(String),
+    Num(&'src str),
+    Str(&'src str),
 
     If,
     Then,
@@ -46,10 +47,10 @@ pub enum Token {
     Pub,
     Mod,
 
-    Comment(String),
+    Comment(&'src str),
 }
 
-impl std::fmt::Display for Token {
+impl std::fmt::Display for Token<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         use Token::*;
 
@@ -80,43 +81,44 @@ impl std::fmt::Display for Token {
     }
 }
 
-pub fn lexer() -> impl Parser<char, Vec<Spanned<Token>>, Error = Simple<char>> {
+pub fn lexer<'src>() -> impl Parser<'src, &'src str, Vec<Spanned<Token<'src>>>> {
     let num = text::int(10)
-        .chain::<char, _, _>(just('.').chain(text::digits(10)).or_not().flatten())
-        .collect::<String>()
+        .then(just('.').then(text::digits(10)).or_not())
+        .slice()
         .map(Token::Num);
 
     // A parser for strings
-    let str_ = just('"')
-        .ignore_then(filter(|c| *c != '"').repeated())
-        .then_ignore(just('"'))
-        .collect::<String>()
+    let str_ = none_of("\\\"")
+        .ignored()
+        .repeated()
+        .slice()
+        .delimited_by(just('"'), just('"'))
         .map(Token::Str);
 
     // A parser for control characters (delimiters, semicolons, etc.)
     let ctrl = one_of("(),=:+.;[]{}|").map(Token::Ctrl);
 
     let operator = {
-        let op_add = just('+').map(|c| Token::Op(c.to_string()));
-        let op_sub = just('-').map(|c| Token::Op(c.to_string()));
-        let op_mul = just('*').map(|c| Token::Op(c.to_string()));
-        let op_div = just('/').map(|c| Token::Op(c.to_string()));
-        let op_rem = just('%').map(|c| Token::Op(c.to_string()));
-        let op_eq = just("==").map(|c| Token::Op(c.to_string()));
-        let op_neq = just("!=").map(|c| Token::Op(c.to_string()));
-        let op_less = just('<').map(|c| Token::Op(c.to_string()));
-        let op_less_eq = just("<=").map(|c| Token::Op(c.to_string()));
-        let op_more = just('>').map(|c| Token::Op(c.to_string()));
-        let op_more_eq = just(">=").map(|c| Token::Op(c.to_string()));
+        let op_add = just("+").map(|c| Token::Op(c));
+        let op_sub = just("-").map(|c| Token::Op(c));
+        let op_mul = just("*").map(|c| Token::Op(c));
+        let op_div = just("/").map(|c| Token::Op(c));
+        let op_rem = just("%").map(|c| Token::Op(c));
+        let op_eq = just("==").map(|c| Token::Op(c));
+        let op_neq = just("!=").map(|c| Token::Op(c));
+        let op_less = just("<").map(|c| Token::Op(c));
+        let op_less_eq = just("<=").map(|c| Token::Op(c));
+        let op_more = just(">").map(|c| Token::Op(c));
+        let op_more_eq = just(">=").map(|c| Token::Op(c));
 
-        let op_bit_and = just('&').map(|c| Token::Op(c.to_string()));
-        let op_logical_and = just("&&").map(|c| Token::Op(c.to_string()));
-        let op_bit_or = just('|').map(|c| Token::Op(c.to_string()));
-        let op_logical_or = just("||").map(|c| Token::Op(c.to_string()));
+        let op_bit_and = just("&").map(|c| Token::Op(c));
+        let op_logical_and = just("&&").map(|c| Token::Op(c));
+        let op_bit_or = just("|").map(|c| Token::Op(c));
+        let op_logical_or = just("||").map(|c| Token::Op(c));
 
-        let op_bit_xor = just('^').map(|c| Token::Op(c.to_string()));
+        let op_bit_xor = just("^").map(|c| Token::Op(c));
 
-        let op_join = just("++").map(|c| Token::Op(c.to_string()));
+        let op_join = just("++").map(|c| Token::Op(c));
 
         op_add
             .or(op_sub)
@@ -137,72 +139,43 @@ pub fn lexer() -> impl Parser<char, Vec<Spanned<Token>>, Error = Simple<char>> {
             .or(op_join)
     };
 
-    let kw_use = just("use").map(|_| Token::Use);
-    let kw_pub = just("pub").map(|_| Token::Pub);
-    let kw_arrow = just("->").map(|_| Token::Arrow);
-    let kw_let = just("let").map(|_| Token::Let);
-    let kw_in = just("in").map(|_| Token::In);
-    let kw_if = just("if").map(|_| Token::If);
-    let kw_then = just("then").map(|_| Token::Then);
-    let kw_else = just("else").map(|_| Token::Else);
-    let kw_true = just("true").map(|_| Token::Bool(true));
-    let kw_false = just("false").map(|_| Token::Bool(false));
-    let kw_where = just("where").map(|_| Token::Where);
-    let kw_match = just("match").map(|_| Token::Match);
-    let kw_when = just("when").map(|_| Token::When);
-    let kw_type = just("type").map(|_| Token::Type);
-    let kw_impl = just("impl").map(|_| Token::Impl);
-    let kw_enum = just("enum").map(|_| Token::Enum);
-    let kw_mod = just("mod").map(|_| Token::Mod);
-    let ident = ident().map(Token::Ident);
+    let ident = ident().map(|ident: &str| {
+        match ident {
+            "->" => Token::Arrow,
+            "if" => Token::If,
+            "then" => Token::Then,
+            "else" => Token::Else,
+            "let" => Token::Let,
+            "in" => Token::In,
+            "where" => Token::Where,
+            "match" => Token::Match,
+            "when" => Token::When,
+            "type" => Token::Type,
+            "impl" => Token::Impl,
+            "enum" => Token::Enum,
+            "use" => Token::Use,
+            "pub" => Token::Pub,
+            "mod" => Token::Mod,
+            "true" => Token::Bool(true),
+            "false" => Token::Bool(false),
 
-    // A single token can be one of the above
-    let token = num
-        .or(str_)
-        .or(kw_use)
-        .or(kw_pub)
-        .or(kw_arrow)
-        .or(kw_let)
-        .or(kw_in)
-        .or(kw_if)
-        .or(kw_then)
-        .or(kw_else)
-        .or(kw_true)
-        .or(kw_false)
-        .or(kw_where)
-        .or(kw_match)
-        .or(kw_when)
-        .or(kw_impl)
-        .or(kw_type)
-        .or(kw_enum)
-        .or(kw_mod)
+            _ => Token::Ident(ident),
+        }
+    });
+
+    let comment = just("#")
+        .then(any().and_is(just('\n').not()).repeated())
+        .padded()
+        .map(|(comment, ())| Token::Comment(comment));
+
+    num.or(str_)
         .or(ctrl)
         .or(operator)
         .or(ident)
-        .recover_with(skip_then_retry_until([]));
-
-    let comment = just("#").then(take_until(just('\n'))).padded();
-
-    token
-        .map_with_span(|tok, span| (tok, span))
-        .padded_by(comment.repeated())
+        .or(comment)
+        .map_with_span(|t, s| (t, s))
         .padded()
+        .recover_with(skip_then_retry_until(any().ignored(), end()))
         .repeated()
-}
-
-fn ident<C: text::Character, E: chumsky::Error<C>>(
-) -> impl Parser<C, C::Collection, Error = E> + Copy + Clone {
-    filter(|c: &C| {
-        let chr = c.to_char();
-        chr.is_ascii_alphabetic() || chr == '_' || chr == '$'
-    })
-    .map(Some)
-    .chain::<C, Vec<_>, _>(
-        filter(|c: &C| {
-            let chr = c.to_char();
-            chr.is_ascii_alphanumeric() || c.to_char() == '_'
-        })
-        .repeated(),
-    )
-    .collect()
+        .collect()
 }
