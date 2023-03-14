c2247359e4681f1a2c71cbf9bbf0d28d05699162
Matthias Beyer <mail@beyermatthias.de>
2023-03-14 13:35:48 +0100

    Inline TypeImpl into Expr
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- vunk-parser/src/ast/decl.rs
+++ vunk-parser/src/ast/decl.rs
@@ -2,9 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use crate::ast::def::Def;
-use crate::ast::generic::WhereClause;
-
 #[derive(Debug)]
 #[cfg_attr(test, derive(PartialEq))]
 pub enum DeclType {
@@ -17,12 +14,4 @@ pub enum DeclType {
 pub struct DeclArg {
     pub name: Option<String>,
     pub ty: DeclType,
-}
-
-#[derive(Debug)]
-#[cfg_attr(test, derive(PartialEq))]
-pub struct TypeImpl {
-    pub name: String,
-    pub generics: Option<WhereClause>,
-    pub members: Vec<Def>,
 }
