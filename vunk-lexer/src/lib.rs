361cd04a8d1b0cb69b06d7bbcf6eaf82f9d26505
Matthias Beyer <mail@beyermatthias.de>
2023-03-17 10:51:17 +0100

    Add tests for keyword tokenizing
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- vunk-lexer/src/lib.rs
+++ vunk-lexer/src/lib.rs
@@ -181,3 +181,54 @@ pub fn lexer<'src>() -> impl Parser<'src, &'src str, Vec<Spanned<Token<'src>>>, 
         .repeated()
         .collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use chumsky::span::SimpleSpan;
+    use chumsky::Parser;
+
+    use super::Token;
+
+    fn lex<'src>(code: &'src str) -> Vec<(Token<'src>, SimpleSpan)> {
+        let lexer = crate::lexer();
+        let lexer_res = lexer.parse(code);
+        let has_errs = lexer_res.has_errors();
+        let errs = lexer_res.errors();
+
+        assert!(
+            !has_errs,
+            "No errors expected, but found: {:?}",
+            errs.collect::<Vec<_>>()
+        );
+        lexer_res.output().unwrap().clone()
+    }
+
+    macro_rules! create_test_for_token {
+        ($name:ident => $exp:ty) => {
+            paste::paste! {
+                #[test]
+                fn [< test_lex_ $name >]() {
+                    let code = stringify!($name);
+                    let tokens = lex(code);
+                    let t = tokens.first().unwrap();
+                    assert!(matches!(t.0, $exp), "Expected {}, got: {:?}", stringify!($exp), t);
+                }
+            }
+        };
+    }
+
+    create_test_for_token!(if => Token::If);
+    create_test_for_token!(then => Token::Then);
+    create_test_for_token!(else => Token::Else);
+    create_test_for_token!(let => Token::Let);
+    create_test_for_token!(in => Token::In);
+    create_test_for_token!(where => Token::Where);
+    create_test_for_token!(match => Token::Match);
+    create_test_for_token!(when => Token::When);
+    create_test_for_token!(type => Token::Type);
+    create_test_for_token!(impl => Token::Impl);
+    create_test_for_token!(enum => Token::Enum);
+    create_test_for_token!(use => Token::Use);
+    create_test_for_token!(pub => Token::Pub);
+    create_test_for_token!(mod => Token::Mod);
+}
