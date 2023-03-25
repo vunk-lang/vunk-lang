a6f265d40e3e4a5e3be2900d9311144eb4c8dbb5
Matthias Beyer <mail@beyermatthias.de>
2023-03-25 15:36:09 +0100

    fixup! Add error output when tokenizer fails

--- vunk-parser/src/expr.rs
+++ vunk-parser/src/expr.rs
@@ -589,7 +589,11 @@ mod tests {
     macro_rules! ast_has_no_errs {
         ($code:ident) => {
             let res = vunk_lexer::lexer().parse($code);
-            assert!(!res.has_errors(), "Error while tokenizing: {:?}", res.errors().collect::<Vec<_>>());
+            assert!(
+                !res.has_errors(),
+                "Error while tokenizing: {:?}",
+                res.errors().collect::<Vec<_>>()
+            );
             let tokens = res.into_output().unwrap();
             let tokens = tokens.as_slice().spanned(($code.len()..$code.len()).into());
             let res = Expr::parser().parse(tokens);
